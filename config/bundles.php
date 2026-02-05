29f5247f39749a35bf2f1c15e57e2605486b5f6d
WolfgangT <wolfgang.tippelt@web.de>
2026-02-05 19:33:06 +0100

    feat(m0): add integration config root and env setup
    Closes #2

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    App\Integration\Infrastructure\Symfony\IntegrationBundle::class => ['all' => true],
 ];
